a626f425ddea0dab8976a59903cf8b3c7d67d4d5
mint <3432747@mail.ru>
2019-03-26 23:30:00 +0300

    Now CPUs are sorted so we have first single cores, then HT, then offline cores

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ struct cpu_t
     int online;
     int package_id;
     int core_id;
+    int location; // bitwised core_id and package_id
 };
 
 struct sysconfig_t
@@ -38,14 +39,16 @@ struct sysconfig_t
     struct cpu_t* cpus;
 };
 
-static int get_topology(struct cpu_t* cpus, int n_proc_conf);
+static int  get_topology(struct cpu_t* cpus, int n_proc_conf);
 
 /*
     Allocates memory for cpus arg. Must be freed before exit (only when
     function returns EXIT_SUCCESS).
  */
-static int get_system_config(struct sysconfig_t* const sys,
-                             struct cpu_t** const cpus);
+static int get_system_config(struct sysconfig_t* const sys);
+static void delete_config(struct sysconfig_t* sys);
+static void sort_cpus(struct sysconfig_t* const sys, struct cpu_t* cpus);
+static int is_used(int cores[], size_t n_cores, int test_core);
 
 int main(int argc, char** argv)
 {
@@ -63,13 +66,18 @@ int main(int argc, char** argv)
     }
 
     struct sysconfig_t sys;
-    struct cpu_t* cpus;
-    if (get_system_config(&sys, &cpus))
+    if (get_system_config(&sys))
     {
         printf("get_system_config() failed\n");
         return EXIT_FAILURE;
     }
     
+
+    for(int i = 0; i < sys.n_proc_conf; i++)
+    {
+        printf("core: %d: %s\n", sys.cpus[i].core_id, (sys.cpus[i].online? "online":"offline"));
+    }
+
 /*
     pthread_t tids[N];
     int buf[N];
@@ -91,6 +99,8 @@ int main(int argc, char** argv)
 
     return 0;
 */
+
+    delete_config(&sys);
 
     return 0;
 }
@@ -131,22 +141,37 @@ static int get_topology(struct cpu_t* cpus, int n_proc_conf)
             cpus[i].online = 1;
         DBG printf("CPU%d: online     = %d\n",   i, cpus[i].online);
 
+        // Offline CPUs do not have topology/
+        if (!cpus[i].online)
+        {
+            cpus[i].core_id    = -1;
+            cpus[i].package_id = -1;
+            cpus[i].location = (cpus[i].package_id << (sizeof(int)/2)) | cpus[i].core_id;
+            
+            DBG printf("CPU%d: core_id    = %d\n",   i, cpus[i].core_id);
+            DBG printf("CPU%d: package id = %d\n",   i, cpus[i].package_id); 
+            DBG printf("CPU%d: location   = %d\n\n", i, cpus[i].location);
+            
+            continue;
+        }
+
         if (get_cpu_attribute(CPU_FILE_PHYS_ID,  i, &cpus[i].core_id))
             return EXIT_FAILURE;
         DBG printf("CPU%d: core_id    = %d\n",   i, cpus[i].core_id);
 
         if (get_cpu_attribute(CPU_FILE_PACKAGE,  i, &cpus[i].package_id))
             return EXIT_FAILURE;
-        DBG printf("CPU%d: package id = %d\n\n", i, cpus[i].package_id);    
+        DBG printf("CPU%d: package id = %d\n", i, cpus[i].package_id);
+
+        cpus[i].location = (cpus[i].package_id << (sizeof(int)/2)) | cpus[i].core_id;
+        DBG printf("CPU%d: location   = %d\n\n", i, cpus[i].location);
     }
     return EXIT_SUCCESS;
 }
 
-static int get_system_config(struct sysconfig_t* const sys,
-                             struct cpu_t** const cpus)
+static int get_system_config(struct sysconfig_t* const sys)
 {
     assert(sys);
-    assert(cpus);
 
     if (read_value(CACHE_LINE, "%lu", &sys->cache_line))
     {
@@ -164,26 +189,74 @@ static int get_system_config(struct sysconfig_t* const sys,
     }
     DBG printf("configured processors:\t%4d\n\n", sys->n_proc_conf);
 
-    *cpus = (struct cpu_t*)calloc(sys->n_proc_conf, sizeof(struct cpu_t));
-    if (!*cpus)
+    struct cpu_t cpus[sys->n_proc_conf];
+    if (get_topology(cpus, sys->n_proc_conf))
+    {
+        printf("Failed to get topology\n");
+        return EXIT_FAILURE;
+    }
+
+    sys->cpus = (struct cpu_t*)calloc(sys->n_proc_conf, sizeof(struct cpu_t));
+    if (!sys->cpus)
     {
         printf("calloc() failed\n");
         return EXIT_FAILURE;
     }
 
-    if (get_topology(*cpus, sys->n_proc_conf))
-    {
-        printf("Failed to get topology\n");
-        free(*cpus);
-        return EXIT_FAILURE;
-    }
-
-
+    sort_cpus(sys, cpus);
 
     return EXIT_SUCCESS;
 }
 
+static void delete_config(struct sysconfig_t* sys)
+{
+    free(sys->cpus);
+}
 
+static void sort_cpus(struct sysconfig_t* const sys, struct cpu_t* cpus)
+{ 
+    // Storing indexes
+    size_t first_used[sys->n_proc_conf];
+    size_t duplicates[sys->n_proc_conf];
+    size_t offline   [sys->n_proc_conf];
+    // Storing locations
+    int    cores_used[sys->n_proc_conf];
+
+    size_t n_first_used = 0;
+    size_t n_duplicates = 0;
+    size_t n_offline    = 0;
+    size_t n_cores_used = 0;
+
+    for(size_t i = 0; i < sys->n_proc_conf; i++)
+    {
+        if (cpus[i].online)
+        {
+            if (!is_used(cores_used, n_cores_used, cpus[i].location))
+            {
+                first_used[n_first_used++] = i;
+                cores_used[n_cores_used++] = cpus[i].location;
+            }
+            else
+                duplicates[n_duplicates++] = i;
+        }
+        else
+            offline[n_offline++] = i;
+    }
+
+    for(size_t i = 0; i < n_first_used; i++)
+        sys->cpus[i] = cpus[first_used[i]];
+    for(size_t i = 0; i < n_duplicates; i++)
+        sys->cpus[n_first_used + i] = cpus[duplicates[i]];
+    for(size_t i = 0; i < n_offline;    i++)
+        sys->cpus[n_first_used + n_duplicates + i] = cpus[offline[i]];
+}
+
+static int is_used(int cores[], size_t n_cores, int test_core)
+{
+    for(size_t i = 0; i < n_cores; i++)
+        if (cores[i] == test_core)  return 1;
+    return 0;
+}
 
 
 
